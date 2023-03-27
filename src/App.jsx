6ff5dc3b0e42d280c58cecb52402f101d42288fa
Oluwafemi Ayinla <oluwafemi.ayinla@gmail.com>
2023-03-27 10:22:04 +0100

    add the Nav component and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>App component</h1>
     </div>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img
+        src="../../public/images/airbnb-logo.png"
+        className="nav--logo"
+        alt="photo-grid"
+      />
+    </nav>
+  );
+}
