1a48a47a17c254c5694c5470b5f57d59341a2613
Oluwafemi Ayinla <oluwafemi.ayinla@gmail.com>
2023-03-27 09:58:58 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App component</h1>
+    </div>
+  );
+}
+
+export default App;
